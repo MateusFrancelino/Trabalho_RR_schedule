7013db0195baf29733a2b1a42d2e4935f2d4956e
MateusFrancelino <32208103+MateusFrancelino@users.noreply.github.com>
2021-05-24 18:50:25 -0300

    Update schedulers.c

--- schedulers.c
+++ schedulers.c
@@ -1,32 +1,46 @@
 #include "schedulers.h"
 
-
+#define QUANTUM 7
 struct node *inicioNodo=NULL;
 int temp=0;
 
 void add(char *name, int priority, int burst){
     Task *taskt = malloc(sizeof(Task));
     taskt->name = name;
+    printf("%s \n",taskt->name);
     taskt->burst = burst;
     taskt->priority = priority;
     insert(&inicioNodo,taskt);
+    //traverse(inicioNodo);
+
 }
 
 
 // invoke the scheduler
 void schedule(){
-struct node *copia=inicioNodo;
+    int currentTime = 0;
+    int taskCounter = 0;
+    float turnAroundSum = 0;
+    float turnAroundAvg = 0;
+
+    struct node *copia=inicioNodo;
 Task *taskt;
     while(inicioNodo != NULL){
-
-        run(inicioNodo->task,10);
-        inicioNodo->task->burst-=10;
-
-        if (inicioNodo->task->burst<=0)
+        if(inicioNodo->task->burst < QUANTUM){
+            currentTime += inicioNodo->task->burst;
+            run(inicioNodo->task,inicioNodo->task->burst);
+            inicioNodo->task->burst -= inicioNodo->task->burst;
+        }
+        else{
+            currentTime += QUANTUM;
+            run(inicioNodo->task,QUANTUM);
+            inicioNodo->task->burst-=QUANTUM;
+        }
+        if (inicioNodo->task->burst<=0){
+            taskCounter += 1;
+            turnAroundSum += currentTime;
             delete(&copia,inicioNodo->task);
-            /*if (taskt->burst>=0)
-                insert(&copia,taskt);*/
-
+        }
         if(inicioNodo->next==NULL){
             inicioNodo=copia;
         }else{
@@ -34,5 +48,7 @@ Task *taskt;
         }
 
     }
+    turnAroundAvg = turnAroundSum/taskCounter;
+    printf("\n Turn Around AVG = %f", turnAroundAvg);
 
 }
